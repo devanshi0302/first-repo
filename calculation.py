9bf62205a9572bd5f5b78206f01a376f9a723eb2
devanshi0302 <devanshiii525.gamil.com>
2025-08-21 21:29:43 +0530

    for multiplication

--- calculation.py
+++ calculation.py
@@ -3,7 +3,9 @@ num2 = float(input("Enter the second number: "))
 
 addition = num1 + num2
 subtraction = num1 - num2
+multiplication = num1 * num2
 
 print("\nResults:")
 print(f"Addition: {num1} + {num2} = {addition}")
 print(f"Subtraction: {num1} - {num2} = {subtraction}")
+print(f"Multiplication: {num1} * {num2} = {multiplication}")
