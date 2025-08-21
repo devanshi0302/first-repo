f3bf97be5ca248b24010f2b0213cf4636d1e5252
devanshi0302 <devanshiii525.gamil.com>
2025-08-21 21:27:24 +0530

    for subtraction

---
+++ calculation.py
@@ -0,0 +1,9 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+
+print("\nResults:")
+print(f"Addition: {num1} + {num2} = {addition}")
+print(f"Subtraction: {num1} - {num2} = {subtraction}")
